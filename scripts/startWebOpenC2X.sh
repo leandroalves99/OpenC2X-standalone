2b5421471cd2512c1d068de6630301527b0ba66b
Leandro Alves <43756835+leandroalves99@users.noreply.github.com>
2022-02-03 10:59:48 -0500

    add script to start web

---
+++ scripts/startWebOpenC2X.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPTS_DIR=`dirname $0`
+#SCRIPTPATH2="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"	
+OPENC2X_DIR=`dirname $SCRIPTS_DIR`
+WEB_DIR=$OPENC2X_DIR/webSite
+
+if [ ! -d $WEB_DIR ]; then
+	echo Web Folder does not exists!
+	exit 0
+fi
+
+cd $WEB_DIR && python3 -m http.server
+
